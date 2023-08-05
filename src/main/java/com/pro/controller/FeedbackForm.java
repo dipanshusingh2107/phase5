87a8c1798c4d7bf5344e8782483ab9c785386ea8
Sushant Kumar <dipanshusingh2107@gmail.com>
2023-08-05 13:44:11 +0530

    Final version with docker compose using phase5 image

--- src/main/java/com/pro/controller/FeedbackForm.java
+++ src/main/java/com/pro/controller/FeedbackForm.java
@@ -1,7 +1,13 @@
 package com.pro.controller;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
 import java.util.Map;
 
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,18 +16,34 @@ import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class FeedbackForm {
-	@GetMapping(path="/")
+	@GetMapping(path = "/")
 	public ModelAndView getFeedbackForm() {
 		ModelAndView view = new ModelAndView("feedbackForm");
 		return view;
 	}
-	
-	
-	@PostMapping(path="/")
-	public String postFeedbackForm(@RequestParam Map<String, String>req) {
+
+	@PostMapping(path = "/")
+	public String postFeedbackForm(@RequestParam Map<String, String> req) {
 		System.out.println(req);
-		
-		return "submitted";
+		JSONObject data = new JSONObject(req);
+
+		try {
+			HttpClient client = HttpClient.newHttpClient();
+			
+			HttpRequest request = HttpRequest.newBuilder()
+					  .POST(BodyPublishers.ofString(data.toString()))
+				      .uri(URI.create("http://localhost:5110"))
+				      .header("Content-Type", "application/json")
+				      .build();
+			
+			client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+			// handle response here...
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			return "submitted";
+		}
+
 	}
 
 }
